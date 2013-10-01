cf7379e82b0d3bae5c2d08124d167319174297ed
Kyle Curren <ksc4130@gmail.com>
2013-10-01 19:41:08 -0400

    btn

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var Gpio = require('onoff').Gpio,
     led = new Gpio(67, 'out'),    // Export GPIO #67 as an output.
-    btn = new Gpio(44, 'in', 'falling'); // Export GPIO #44 as an interrupt
+    btn = new Gpio(44, 'in', 'both'); // Export GPIO #44 as an interrupt
 
 btn.watch(function (err, value) {
     if (err) throw err;
 
     console.log('Button pressed!, its value was ' + value);
-    led.writeSync(led.readSync() === 0 ? 1 : 0); // 1 = on, 0 = off :)
+    if(value === 0)
+        led.writeSync(led.readSync() === 0 ? 1 : 0); // 1 = on, 0 = off :)
     //btn.unexport(); // Unexport GPIO and free resources
 });
