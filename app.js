7a614ad85c6308d6568bffd895bf4cd7a680f041
Kyle Curren <ksc4130@gmail.com>
2013-10-01 20:33:33 -0400

    btn

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-var Gpio = require('onoff').Gpio,
-    led = new Gpio(67, 'out'),    // Export GPIO #67 as an output.
-    btn = new Gpio(44, 'in', 'falling', {
-        persistentWatch: true
-    }); // Export GPIO #44 as an interrupt
+//var Gpio = require('onoff').Gpio,
+//    led = new Gpio(67, 'out'),    // Export GPIO #67 as an output.
+//    btn = new Gpio(44, 'in', 'falling', {
+//        persistentWatch: true
+//    }); // Export GPIO #44 as an interrupt
+//
+//led.writeSync(0);
+//
+//btn.watch(function (err, value) {
+//    if (err) throw err;
+//
+//    console.log('Button pressed!, its value was ' + value);
+//    if(value === 0)
+//        led.writeSync(led.readSync() === 0 ? 1 : 0); // 1 = on, 0 = off :)
+//    //btn.unexport(); // Unexport GPIO and free resources
+//});
 
-led.writeSync(0);
+var b = require('bonescript');
 
-btn.watch(function (err, value) {
-    if (err) throw err;
+var led = 'P9_8',
+    photo = 'P8_36';
 
-    console.log('Button pressed!, its value was ' + value);
-    if(value === 0)
-        led.writeSync(led.readSync() === 0 ? 1 : 0); // 1 = on, 0 = off :)
-    //btn.unexport(); // Unexport GPIO and free resources
-});
+pinMode(led, 'out');
+pinMode(photo, 'in');
+
+setInterval(function (x) {
+    if(x.err)
+        console.log('error', x.err);
+    else
+        console.log(x.value);
+}, 500);
