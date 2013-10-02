d82b53aff266339747f74bf11398b1ca8c97160f
Kyle Curren <ksc4130@gmail.com>
2013-10-02 13:55:06 -0400

    btn

--- app.js
+++ app.js
@@ -15,19 +15,94 @@
 //    //btn.unexport(); // Unexport GPIO and free resources
 //});
 
+var idDeviceCnt = 0;
+var idGroupCnt = 0;
+
+var ob = function (o) {
+    var F = function () {};
+    F.prototype = o || {};
+    var f = new F();
+    return f;
+};
+
+var group = function (args) {
+    var self = ob();
+
+    args = args || {};
+
+    self.id = idGroupCnt++;
+    self.name = args.name;
+    self.childGroups = args.childGroups || [];
+    self.devices = args.devices || [];
+
+    return self;
+};
+
+var devicePrototype = {
+    toggle: function (state) {
+        var self = this;
+
+        if(self.actionType === 'onoff') {
+            if(state) {
+                b.digitalWrite(self.pin, state);
+            } else {
+                b.digitalWrite(self.pin, (1 - self.state || 0));
+            }
+        }
+    }
+};
+
+var device = function (pin, args) {
+    if(!pin || typeof pin !== 'string')
+        return null;
+
+    var self = ob(devicePrototype);
+
+    args = args || {};
+
+    self.id = idDeviceCnt++;
+    self.actionType = args.actionType;
+    self.type = args.type;
+    self.pin = args.pin;
+    self.name = args.name || 'untitled';
+    self.state = args.state;
+    self.controls = args.controls;
+
+    if(args.actionType && args.actionType === 'onoff') {
+        b.pinMode(args.pin, 'out');
+        b.digitalWrite(self.pin, (self.state || 0));
+    }
+
+    return self;
+};
+
+var devices = {};
+var groups = {};
+
+devices['1'] = device({
+    name: 'led',
+    actionType: 'onoff',
+    type: 'light',
+    pin: 'P8_8',
+    state: 0
+});
+
+
+
 var b = require('bonescript');
 
-var led = 'P8_8',
-    photo = 'P9_36';
+//var led = 'P8_8',
+//    photo = 'P9_36';
 
-b.pinMode(led, 'out');
+//b.pinMode(led, 'out');
 //b.pinMode(photo, 'in');
 
 setInterval(function () {
-    b.analogRead(photo, function (x) {
-        if(x.err)
-            console.log('error', x.err);
-        else
-            console.log(x.value);
-    });
+    devices['1'].toggle();
+//    b.analogRead(photo, function (x) {
+//        if(x.err)
+//            console.log('error', x.err);
+//        else
+//            console.log(x.value);
+//    });
 }, 500);
