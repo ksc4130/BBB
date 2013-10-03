17aedc7f32351af2f1648d5d3f75118ee34c73e5
Kyle Curren <ksc4130@gmail.com>
2013-10-02 23:58:10 -0400

    btn

--- app.js
+++ app.js
@@ -33,11 +33,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
-
-app.post('/yup', function(req, res){
-
-});
+//app.get('/users', user.list);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
